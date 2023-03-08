fa1fbfcadf5fd2f22977018207323a9a0f342a2a
nancyk12 <nancyk12@hotmail.com>
2023-03-08 12:42:26 -0500

    updated port to 3008 and fixed error in blog.js

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 // makes out .env file available to be used in the whole application
-require('dotenv').config();
+require("dotenv").config();
 //connects our applications to mongodb atlas
-const { mongoConnect } = require('./mongo');
+const { mongoConnect } = require("./mongo");
 mongoConnect();
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const blogsRouter = require('./routes/blogs');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const blogsRouter = require("./routes/blogs");
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 //app.get is only used in the routes file
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json()); //helps us get the JSON data
 app.use(express.urlencoded({ extended: false })); //helps us get the JSON data
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 app.use("/blogs", blogsRouter );
 
 // catch 404 and forward to error handler
@@ -38,11 +38,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- routes/blogs.js
+++ routes/blogs.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const router = express.Router();
-// {}destructuring of the object, grabbing db and making its own variable
+// {} destructuring of the object, grabbing db and making its own variable
 //../ leaving folder, ./leaving file
 const { db } = require("../mongo");
 
-router.get("./", function (req, res) {
+router.get("/", function (req, res) {
     res.json({message: "Hello World"});
 });
 //anytime dealing with a database, you need an async function
 router.get("/all", async function(req, res) {
-    const getAllBlogs = await db().collection('sample_blogs').find();
+    const getAllBlogs = await db()
+    .collection('sample_blogs')
+    .find().toArray();
     res.json({ blogs: getAllBlogs });
 });
 
@@ -21,12 +23,11 @@ router.post("/create-one", async function(req, res) {
         author,
         categories,
         createdAt: new Date().toISOString(),
-        lastModified: new Date().toISOString()
-    }
+        lastModified: new Date().toISOString(),
+    };
     await db().collection("sample_blogs").insertOne(newData);
     res.json({success: true, message: "added"});
 
 });
-
 
     module.exports = router;
